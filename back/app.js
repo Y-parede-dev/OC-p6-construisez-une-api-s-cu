1104380dc607f51da302267410fa6a78785d03b6
yoan parede <parede.yoan.amcs@gmail.com>
2021-01-12 14:19:20 +0100

    remove comment ligne

--- back/app.js
+++ back/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path')
-//const fs = require('fs');
 
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
@@ -14,11 +13,6 @@ mongoose.connect( "mongodb+srv://admin1:root@p6-construire-une-api-s.y3ru5.mongo
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
-/* On récupère notre clé privée et notre certificat (ici ils se trouvent dans le dossier certificate) 
-const key = fs.readFileSync(path.join(__dirname, 'certificate', 'server.key'));
-const cert = fs.readFileSync(path.join(__dirname, 'certificate', 'server.cert'));
-const options = { key, cert };
-*/
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -31,5 +25,5 @@ app.use(bodyParser.json())
 app.use('/images', express.static(path.join(__dirname, "images")));
 app.use("/api/sauces", saucesRoutes);
 app.use("/api/auth", userRoutes);
-//module.exports = options;
+
 module.exports = app;
